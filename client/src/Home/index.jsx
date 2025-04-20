fbf4830c0146728689be6ed4a4399187b862921e
anuragK-24 <24sept.anurag@gmail.com>
2025-04-20 13:32:47 +0530

    Added the tailwindcss into the project successfully

--- client/src/Home/index.jsx
+++ client/src/Home/index.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 
 function Home() {
-  return <div>Home</div>;
+  return <div className="flex justify-center">
+    <h1 className="bg-red-500 w-100 h-10">Home</h1>
+    </div>;
 }
 
 export default Home;
